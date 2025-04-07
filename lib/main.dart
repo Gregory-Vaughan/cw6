f3be4d94ad3a100c1ab4b57ad0e101ed92e21db0
Gregory-Vaughan <gpv520@gmail.com>
2025-04-07 18:44:13 -0400

    got the basics done. forgot to add to github.

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task Manager',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Task Manager')),
+        body: const Center(child: Text('Hello Firebase!')),
+      ),
+    );
+  }
+}
